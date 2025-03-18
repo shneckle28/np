c4b1e26aaa3cd1511f74081fa496bcf55725b6c6
shneckle28 <noahmilliken123@gmail.com>
2025-03-18 19:50:17 -0400

    Updated Blog Post

--- main.js
+++ main.js
@@ -183,7 +183,7 @@ const paperMaterial = new THREE.MeshStandardMaterial({
 
 // Create the paper mesh
 const blogPaper = new THREE.Mesh(paperGeometry, paperMaterial);
-blogPaper.position.set(0, 2.5, 4.5);
+blogPaper.position.set(-0.7, 2.5, 4.7);
 blogPaper.rotation.y = Math.PI / 0.1;
 blogPaper.userData.clickable = true; // Enable interaction
 scene.add(blogPaper);
